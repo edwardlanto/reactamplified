721ca86b24fef20b4336b7114efca08a752a82c4
Edward Lanto <elanto@hotmail.com>
2020-11-01 23:14:04 -0800

    added create post and render posts

--- src/App.js
+++ src/App.js
@@ -1,58 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import Amplify, { API, Auth, Storage } from 'aws-amplify'
-import { v4 as uuid } from 'uuid'
-import { listPosts } from './graphql/queries'
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+import React, { useState, useEffect } from "react";
+import {
+  HashRouter,
+  Switch,
+  Route
+} from "react-router-dom";
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { css } from 'emotion';
+import { API, Storage, Auth } from 'aws-amplify';
+import { listPosts } from './graphql/queries';
 
-function App() {
-  const [posts, setPosts] = useState([])
+import Posts from './Posts';
+import Post from './Post';
+import Header from './Header';
+import CreatePost from './CreatePost';
+import Button from './Button';
 
+function Router() {
+  /* create a couple of pieces of initial state */
+  const [showOverlay, updateOverlayVisibility] = useState(false);
+  const [posts, updatePosts] = useState([]);
+  const [myPosts, updateMyPosts] = useState([]);
 
+  /* fetch posts when component loads */
   useEffect(() => {
-    fetchPosts();
-    checkUser();
+      fetchPosts();
   }, []);
-
-  async function checkUser() {
-    const user = await Auth.currentAuthenticatedUser();
-    console.log('user: ', user);
-    console.log('user attributes: ', user.attributes);
-  }
-
-
   async function fetchPosts() {
-    try {
-      const postData = await API.graphql({ query: listPosts });
-      setPosts(postData.data.listPosts.items)
-    } catch (err) {
-      console.log({ err })
-    }
+    /* query the API, ask for 100 items */
+    let postData = await API.graphql({ query: listPosts, variables: { limit: 100 }});
+    let postsArray = postData.data.listPosts.items;
+    /* map over the image keys in the posts array, get signed image URLs for each image */
+    postsArray = await Promise.all(postsArray.map(async post => {
+      const imageKey = await Storage.get(post.image);
+      post.image = imageKey;
+      return post;
+    }));
+    /* update the posts array in the local state */
+    setPostState(postsArray);
+  }
+  async function setPostState(postsArray) {
+    const user = await Auth.currentAuthenticatedUser();
+    const myPostData = postsArray.filter(p => p.owner === user.username);
+    console.log('postsArray:' , postsArray)
+    updateMyPosts(myPostData);
+    updatePosts(postsArray);
   }
   return (
-    <div>
-      <h1>Hello World</h1>
-      {
-        posts.map(post => (
-          <div key={post.id}>
-            <h3>{post.name}</h3>
-            <p>{post.location}</p>
+    <>
+      <HashRouter>
+          <div className={contentStyle}>
+            <Header />
+            <hr className={dividerStyle} />
+            <Button title="New Post" onClick={() => updateOverlayVisibility(true)} />
+            <Switch>
+              <Route exact path="/" >
+                <Posts posts={posts} />
+              </Route>
+              <Route path="/post/:id" >
+                <Post />
+              </Route>
+              <Route exact path="/myposts" >
+                <Posts posts={myPosts} />
+              </Route>
+            </Switch>
           </div>
-        ))
-      }
-      <AmplifySignOut />
-    </div>
-  )
+          <AmplifySignOut />
+        </HashRouter>
+        { showOverlay && (
+          <CreatePost
+            updateOverlayVisibility={updateOverlayVisibility}
+            updatePosts={setPostState}
+            posts={posts}
+          />
+        )}
+    </>
+  );
 }
 
-const styles = {
-  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: {  marginBottom: 15 },
-  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
-  todoName: { fontSize: 20, fontWeight: 'bold' },
-  todoDescription: { marginBottom: 0 },
-  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
-}
+const dividerStyle = css`
+  margin-top: 15px;
+`
 
-export default withAuthenticator(App);
+const contentStyle = css`
+  min-height: calc(100vh - 45px);
+  padding: 0px 40px;
+`
+
+export default withAuthenticator(Router);
